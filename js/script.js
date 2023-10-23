fd5fdeaa4982d1888a6597f83d7dd1f1b1b49d03
“rcteenx” <rcteenx@gmail.com>
2023-10-23 15:27:00 +0300

    finish mobile menu

--- js/script.js
+++ js/script.js
@@ -38,3 +38,15 @@ function validURL(str) {
   );
   return !!pattern.test(str);
 }
+
+// Hamburger Menu
+const btn = document.getElementById("menu-btn");
+const menu = document.getElementById("menu");
+
+btn.addEventListener("click", navToggle);
+
+function navToggle() {
+  btn.classList.toggle("open");
+  menu.classList.toggle("flex");
+  menu.classList.toggle("hidden");
+}
