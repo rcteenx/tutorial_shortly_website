a1fdc5f4af83494883da0fc0f8f9e2e156933636
“rcteenx” <rcteenx@gmail.com>
2023-10-23 14:32:24 +0300

    add javascript link validation

---
+++ js/script.js
@@ -0,0 +1,40 @@
+const input = document.getElementById("link-input");
+const linkForm = document.getElementById("link-form");
+const errMsg = document.getElementById("err-msg");
+
+linkForm.addEventListener("submit", formSubmit);
+
+function formSubmit(e) {
+  e.preventDefault();
+  if (input.value === "") {
+    errMsg.innerHTML = "Please enter text";
+    errMsg.classList.remove("text-green-500");
+    errMsg.classList.add("text-red");
+    input.classList.add("border-red");
+  } else if (!validURL(input.value)) {
+    errMsg.innerHTML = "Please enter a valid URL";
+    errMsg.classList.remove("text-green-500");
+    errMsg.classList.add("text-red");
+    input.classList.add("border-red");
+  } else {
+    errMsg.innerHTML = "Submitted";
+    errMsg.classList.remove("text-red");
+    errMsg.classList.add("text-green-500");
+    input.value = "";
+    input.classList.remove("border-red");
+  }
+}
+
+// Validate a URL
+function validURL(str) {
+  var pattern = new RegExp(
+    "^(https?:\\/\\/)?" + // protocol
+      "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" +
+      "((\\d{1,3}\\.){3}\\d{1,3}))" +
+      "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" +
+      "(\\?[;&a-z\\d%_.~+=-]*)?" +
+      "(\\#[-a-z\\d_]*)?$",
+    "i"
+  );
+  return !!pattern.test(str);
+}
